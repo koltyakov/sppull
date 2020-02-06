9a5ee3f093fde5ef41f0efabb36f7fc15d999ebf
ARVO\andrew.koltyakov <andrew.koltyakov@arvosys.com>
2020-02-06 12:19:01 +0300

    CAML sample

---
+++ test/manual/caml.ts
@@ -0,0 +1,34 @@
+import { join } from 'path';
+
+import { AuthConfig as SPAuthConfigurator, IAuthConfigSettings } from 'node-sp-auth-config';
+import { ISPPullOptions, ISPPullContext, Download as IDownload } from '../../src';
+
+const Download: IDownload = require(join(__dirname, '../../src'));
+const sppull = Download.sppull;
+
+const authSettings: IAuthConfigSettings = {
+  configPath: './config/integration/private.spo.json'
+};
+
+new SPAuthConfigurator(authSettings).getContext()
+  .then(({ siteUrl, authOptions }) => {
+    const pullContext: ISPPullContext = { siteUrl, ...authOptions } as any;
+    const pullOptions: ISPPullOptions = {
+      spBaseFolder: '/',
+      spRootFolder: 'Shared%20Documents',
+      dlRootFolder: join(__dirname, 'Downloads'),
+      metaFields: [ 'Title' ],
+      camlCondition: `
+        <Geq>
+          <FieldRef Name='Modified' />
+          <Value Type="DateTime">
+              <Today OffsetDays="-30" />
+          </Value>
+        </Geq>
+      `
+    };
+    return sppull(pullContext, pullOptions);
+  })
+  .then(console.log)
+  .then(_ => console.log('Done'))
+  .catch(console.log);
