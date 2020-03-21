eb27926dc16482c4650da42f1e1d5e4a8f5c99d1
Andrew Koltyakov <andrew.koltyakov@gmail.com>
2020-03-21 12:14:46 +0300

    Example update

--- examples/basic/src/index.ts
+++ examples/basic/src/index.ts
@@ -1,21 +1,22 @@
 import { AuthConfig as SPAuthConfigurator } from 'node-sp-auth-config';
 import { ISPPullOptions, ISPPullContext, Download as IDownload } from 'sppull';
 
-const Download: IDownload = require('sppull');
-const sppull = Download.sppull;
+new SPAuthConfigurator().getContext().then((context) => {
 
-(new SPAuthConfigurator()).getContext().then((context) => {
+  const Download: IDownload = require('sppull');
+  const sppull = Download.sppull;
 
-  let pullContext: ISPPullContext = {
+  const pullContext: ISPPullContext = {
     siteUrl: context.siteUrl,
     ...context.authOptions
   } as any;
 
-  let pullOptions: ISPPullOptions = {
+  const pullOptions: ISPPullOptions = {
     spRootFolder: 'Shared%20Documents',
     dlRootFolder: './Downloads/Documents'
   };
 
   sppull(pullContext, pullOptions);
 
+// tslint:disable-next-line: no-console
 }).catch(console.log);
